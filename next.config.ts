63f75148bbab09324b6aa171836be6e8c7c85cbb
dwihp2 <m.dwiheryanto2@gmail.com>
2026-08-09 19:50:50 +0700

    feat: add support for Unsplash images in remote patterns

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "*.cloudinary.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
   serverExternalPackages: ['drizzle-kit', 'drizzle-kit*', 'pg', 'pg-cloudflare'],
